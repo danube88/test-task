d117a6b61386f618b8f02d19eb5139e166b79659
danube88 <36491066+danube88@users.noreply.github.com>
2019-07-05 01:09:54 +0300

    Update PositionsTableSeeder.php

--- database/seeds/PositionsTableSeeder.php
+++ database/seeds/PositionsTableSeeder.php
@@ -15,12 +15,27 @@ class PositionsTableSeeder extends Seeder
         //
         $faker = \Faker\Factory::create('ru_RU');
         for ($i = 0; $i < 100; $i++) {
-          Position::firstOrCreate([
-              'name_position' => $faker->jobTitle,
-              'default_salary' => $faker->numberBetween(0,500000),
-              'admin_created_id' => '1',
-              'admin_updated_id' => '1',
-          ]);
+
+            if ($i == 0) {
+                $salary = 500000;
+            } elseif ($i > 1 && $i <= 20) {
+                $salary = rand(400000,495000);
+            } elseif ($i > 21 && $i <= 50) {
+                $salary = rand(300000,395000);
+            } elseif ($i > 51 && $i <= 70) {
+                $salary = rand(200000,295000);
+            } elseif ($i > 71 && $i <= 80) {
+                $salary = rand(100000,195000);
+            } else {
+                $salary = rand(50000,95000);
+            }
+
+            Position::firstOrCreate([
+                'name_position' => $faker->jobTitle,
+                'default_salary' => $salary,
+                'admin_created_id' => '1',
+                'admin_updated_id' => '1',
+            ]);
         }
     }
 }
